7011a60a6383891b83ff34a6f7671cb8e3fd73b5
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-05-14 13:30:28 -0500

    Fix nav

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -14,9 +14,9 @@ const FeaturedProducts = () => {
             Nuevos productos
           </span>
           <p className="text-2xl-regular text-gray-900 max-w-lg mb-4">
-            Our newest styles are here to help you look your best.
+            Nuestros nuevos productos están aqui para ayudarte con tu nutrición.
           </p>
-          <UnderlineLink href="/store">Explore products</UnderlineLink>
+          <UnderlineLink href="/store">Explorar productos</UnderlineLink>
         </div>
         <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-4 gap-y-8">
           {data
--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -25,7 +25,7 @@ const CartDropdown = () => {
         <Link href="/cart" passHref>
           <Popover.Button className="h-full relative">
             <BsBag className="w-5 h-5" />
-            <p className="bagCount absolute -top-2 -right-2 w-4 bg-slate-400 rounded-full">{totalItems}</p>
+            <p className="bagCount absolute top-2 -right-2 w-4 bg-slate-400 rounded-full">{totalItems}</p>
           </Popover.Button>
         </Link>
         <Transition
